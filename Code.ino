738bdea8a3dfcc23134aa085b2c5eac6565efda1
Trương Quang Tùng <82410379+tunftruonw@users.noreply.github.com>
2023-07-31 11:51:29 +0700

    Final Code

---
+++ Code.ino
@@ -0,0 +1,153 @@
+/* 
+  Based on the example "Example8_SPO2" of library "SparkFun_MAX3010x_Sensor_Library" by Arduino Minh Quang
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+*/
+
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+MAX30105 particleSensor;
+
+#define MAX_BRIGHTNESS        255
+#define MAX_ERROR_INF         0.5  
+#define MAX_ERROR_SUP         1.5        
+#define TIME_RESET            1000    //ms
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+uint32_t timerReset;
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int32_t spo2_Offset=0; //SPO2 error ofset
+int32_t memSpo2 = 100;
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int32_t heartRate_Offset=20; //heart error offset rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+float DiffReadings;
+
+byte readLED = 13; //Blinks with each data read
+
+void setup()
+{
+  lcd.init();
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("DANG KHOI DONG");
+  lcd.setCursor(0, 1);
+  lcd.print("VUI LONG CHO...");
+
+  pinMode(readLED, OUTPUT);
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("LOI KET NOI");
+    lcd.setCursor(0, 1);
+    lcd.print("KT CAM BIEN");
+    while (1);
+  }
+
+  byte ledBrightness = 60; //def: 60, Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 200; //def:100 Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+}
+
+void loop()
+{
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    }
+
+    if (millis() > (timerReset + TIME_RESET))
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      DiffReadings = spo2 / memSpo2;
+      if ((spo2 < 0))//)||(DiffReadings > MAX_ERROR_SUP)||(DiffReadings < MAX_ERROR_INF))
+      {
+        lcd.print("HAY DAT TAY");
+        lcd.setCursor(0, 1);
+        lcd.print("LEN CAM BIEN");
+        timerReset = millis();
+      }
+      else
+      {
+        spo2=spo2-spo2_Offset;
+        heartRate=heartRate-heartRate_Offset;
+        lcd.setCursor(0, 0);
+        lcd.print("NONG DO OXY= ");
+        lcd.print(spo2, DEC);
+        lcd.setCursor(0, 1);
+        lcd.print("NHIP TIM=");
+        lcd.print(heartRate, DEC);
+      }
+      memSpo2 = spo2;
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  }
+}
